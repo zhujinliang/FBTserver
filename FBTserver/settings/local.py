d151e308baec17d790eba3c7bf625ba6c3711340
Jinliang Zhu <jzhu@redhat.com>
2013-06-26 00:35:48 +0800

    Initial the project

---
+++ FBTserver/settings/local.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from FBTserver.settings.default import *
+except ImportError:
+    pass
+
+# Specify custom settings here.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'FBTserver',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+
---
+++ runserver.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python manage.py runserver localhost:8000 --settings=FBTserver.settings.local
